2a9d1d55351c4565ddc93f5c2789cdc1455d630b
Mohhabatdilsa <108210774+Mohhabatdilsa@users.noreply.github.com>
2024-10-15 14:15:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,24 +6,24 @@ const chalk = require('chalk')
 global.sessionid ='put your session id here'
 
 //owmner v card
-global.ytname = "YT: ELITE-PRO-V2" //ur yt chanel name
-global.socialm = "GitHub: Elite-MD" //ur github or insta name
+global.ytname = "YT: DZ OFFICIAL 4K" //ur yt chanel name
+global.socialm = "GitHub: mohabbatdilsa" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'QISMAT-4X' //ur bot name
+global.ownernumber = '918877634151' //ur owner number
+global.ownername = 'QISMAT-4X' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
+global.wm = "QISMAT-PRO-4X"
 global.botscript = 'https://github.com/Elite-MD/ELITE-PRO-V2' //script link
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2"
-global.creator = "2348109263390@s.whatsapp.net"
+global.author = "QISMAT-PRO-4X"
+global.creator = "918877634151@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["918877634151"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -31,7 +31,7 @@ global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
 global.adminevent = false //show promote/demote message
